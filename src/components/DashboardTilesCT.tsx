1855d61769ebea1ecbe6e8d533f23659eca2e37b
kohcharles <charles@creatual.io>
2025-10-10 00:08:56 +0800

    new driver

--- src/components/DashboardTilesCT.tsx
+++ src/components/DashboardTilesCT.tsx
@@ -22,7 +22,7 @@ export default function DashboardTilesCT() {
     </div>
     </Link> */}
 
-    <Link className="flex mt-4 w-full md:w-[300px] items-center gap-x-4 rounded-2xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10" href="/staff">
+    <Link className="flex mt-4 w-full md:w-[300px] items-center gap-x-4 rounded-2xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10" href="/driver">
     <UserPlusIcon aria-hidden="true" className="size-8 text-slate-700" />
 
     <div>
